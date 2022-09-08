7c8e55c026c52b8c09dc82cb2977c3444887a302
Bene <benedikt.binger@gmail.com>
2022-09-08 22:00:24 +0200

    added wallpaper install script

---
+++ wallpapers.sh
@@ -0,0 +1,10 @@
+pacman -S --noconfirm\
+	plasma-workspace-wallpapers\
+	archlinux-wallpaper\
+	cutefish-wallpapers\
+	deepin-community-wallpapers\
+	deepin-wallpapers\
+	deepin-wallpapers-plasma\
+	elementary-wallpapers\
+	gnome-backgrounds\
+	mate-backgrounds
